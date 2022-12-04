583af105e6a64d0e32eb11c82b1e04d5ab14e35b
Sardemberg <lucassardem@gmail.com>
2022-12-03 22:36:35 -0300

    Commit update

--- main.asm
+++ main.asm
@@ -10,6 +10,7 @@
 
 mostrar_questao_um:
 	LDI primeiro_ptr
+	SUB last_caracter
 	JZ responder_questao_um
 	INT output
 
@@ -25,9 +26,41 @@ responder_questao_um:
 
 	MOV 0
 	SUB escolha
-	JN checar_questao_um
+	JN checar_resposta
 
 	JMP responder_questao_um
+
+checar_resposta:
+	LDA escolha
+	SUB resposta
+	JZ resposta_correta
+	JMP valor_final
+
+resposta_correta:
+	LDI ptr_acerto
+	INT output
+
+	MOV 1
+	ADD ptr_acerto
+	STA ptr_acerto
+
+	LDI ptr_acerto
+	STA valor_caracter
+	LDA last_caracter
+	SUB valor_caracter
+
+	JN resposta_correta
+
+	MOV 1
+	ADD acertos
+	STA acertos
+	JMP end
+
+valor_final:
+	LDA mask_output
+	ADD acertos
+	INT output
+	JMP end
 
 end:
 	INT exit
@@ -37,17 +70,25 @@ end:
 	exit: DD 25
 	input: DD 20
 	mask_input: DD 0x100
+	last_caracter: DD 0
 
-	primeira_questao: INITB "Onde rufino bebe?", "1 - Preta", "2 - Tonhao", "3 - Vermelho", 0
-	primeira_resposta: DD 49
+	primeira_questao: INITB "Onde rufino bebe?", " 1 - Preta", " 2 - Tonhao", " 3 - Vermelho", 0
+	resposta: DD 49
 	primeiro_ptr: DD primeira_questao
+
+	acerto: INITB "Acertou", 0
+	ptr_acerto: DD acerto
+	valor_caracter: DD 0
 
 	output: DD 21
 	questao: DD 1
 	mask_output: DD 0x000
 	opcao: DD 49
+
+	acertos: DD 0
 	
 .bss
 	escolha: RESD 1
 
 .stack 10
+
